2867cd6076b8f0e16d8ff6c726f0a6b3d7425aaf
Will Nilges <wdn5796@rit.edu>
2023-08-04 10:24:23 -0400

    Break into multiple files
    At some point I need to make this platform agnostic—sooner rather than
    later. This is the first step to that.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# UNDER CONSTRUCTION PLEASE HOLD
+# GRAB
 
-You MUST use `<wiki url>/api.php` to point to your wiki!!!
+Grab is the backend for a slackbot that you can mention to save a transcript of a thread from Slack to your MediaWiki instance. It is currently under development and absolutely not ready for public consumption. Check the [issues](https://github.com/WillNilges/grab/issues) tab for progress on development.
+
+## Handy tips
+
+In the `.env` file, You MUST use `<wiki url>/api.php` to point to your wiki!!!
---
+++ mediawiki.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+    "fmt"
+)
+
+// Helper function for putting things on the wiki. Can easily control how content
+// gets published by setting or removing variables
+func publishToWiki(append bool, title string, sectionTitle string, convo string) (err error) {
+	// Push conversation to the wiki, (overwriting whatever was already there, if Grab was the only person to edit?)
+	parameters := map[string]string{
+		"action":  "edit",
+		"title":   title,
+		"text":    convo,
+		"bot":     "true",
+		"summary": "Grab publishToWiki",
+	}
+
+
+	if sectionTitle != "" {
+		if append {
+			index, err := findSectionId(title, sectionTitle)
+			if err != nil {
+				return err
+			}
+			parameters["section"] = index 
+		} else {
+			parameters["section"] = "new"
+		}
+		parameters["sectiontitle"] = sectionTitle
+	}
+
+	if append {
+		convo = "\n\n" + convo // Prepend some newlines so that he gets formatted properly
+		delete(parameters, "text")
+		parameters["appendtext"] = convo
+		parameters["summary"] = fmt.Sprintf("Grab publishToWiki append section %s", sectionTitle)
+	}
+
+	// Make the request.
+	return w.Edit(parameters)
+}
+
+func getArticleURL(title string) (url string, missing bool, err error) {
+	newArticleParameters := map[string]string{
+		"action": "query",
+		"format": "json",
+		"titles": title,
+		"prop":   "info",
+		"inprop": "url",
+	}
+
+	newArticle, err := w.Get(newArticleParameters)
+	if err != nil {
+		return "", false, err
+	}
+
+	fmt.Println("CHECKING FOR ARTICLE")
+	fmt.Println(newArticle)
+
+	pages, err := newArticle.GetObjectArray("query", "pages")
+	for _, page := range pages {
+		url, err = page.GetString("canonicalurl")
+		missing, _ = page.GetBoolean("missing")
+		break // Just get first one. There won't ever not be just one.
+	}
+
+	if err != nil {
+		return "", false, err
+	}
+
+	return url, missing, nil
+}
+
+// Check if the section exists or not, that's really all we care about (for now).
+func sectionExists(title string, section string) (exists bool, err error) {
+	sectionQueryParameters := map[string]string{
+		"format": "json",
+		"action": "parse",
+		"page":   title,
+		"prop":   "sections",
+	}
+
+	sectionQuery, err := w.Get(sectionQueryParameters)
+	if err != nil {
+		return false, err
+	}
+
+	sections, err := sectionQuery.GetObjectArray("parse", "sections")
+	for _, sect := range sections {
+		var line string
+		line, err = sect.GetString("line")
+		if err != nil {
+			return false, err
+		}
+		if line == section {
+			return true, nil
+		}
+	}
+
+	return false, nil
+}
+
+// Check if the section exists or not, that's really all we care about (for now).
+func findSectionId(title string, section string) (id string, err error) {
+	sectionQueryParameters := map[string]string{
+		"format": "json",
+		"action": "parse",
+		"page":   title,
+		"prop":   "sections",
+	}
+
+	sectionQuery, err := w.Get(sectionQueryParameters)
+	if err != nil {
+		return "", err
+	}
+
+	sections, err := sectionQuery.GetObjectArray("parse", "sections")
+	for _, sect := range sections {
+		var line string
+		line, err = sect.GetString("line")
+		if err != nil {
+			return "", err
+		}
+		if line == section {
+			id, err = sect.GetString("index") 
+			if err != nil {
+				return "", err
+			}
+			return id, nil
+		}
+	}
+
+	return "", nil
+}
+
