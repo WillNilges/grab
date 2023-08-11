43edd1cf134d9087081c5b95f81f506250bdb2ee
Will Nilges <wdn5796@rit.edu>
2023-08-11 16:01:43 -0400

    Create table if it doesn't exist

--- db.go
+++ db.go
@@ -1,27 +1,37 @@
 package main
 
 import (
-	"github.com/uptrace/bun"
-	"fmt"
 	"context"
+	"fmt"
+
+	"github.com/uptrace/bun"
 )
 
 type Instance struct {
-		SlackTeamID []byte
-		SlackAccessToken []byte
-		MediaWikiURL []byte
-		MediaWikiUname []byte
-		MediaWikiPword []byte
-		MediaWikiDomain []byte
+	GrabID           int
+	SlackTeamID      []byte
+	SlackAccessToken []byte
+	MediaWikiURL     []byte
+	MediaWikiUname   []byte
+	MediaWikiPword   []byte
+	MediaWikiDomain  []byte
 }
 
 // Check if we need to initialize the database, and do so if that's the case
 func initDB(db *bun.DB) (err error) {
 	ctx := context.Background()
-	err = db.ResetModel(ctx, &Instance{})
+
+	instance := new(Instance)
+	/*_, err = db.NewDropTable().Model(instance).IfExists().Exec(ctx)
 	if err != nil {
-		return err
+		panic(err)
+	}*/
+
+	_, err = db.NewCreateTable().Model(instance).IfNotExists().Exec(ctx)
+	if err != nil {
+		panic(err)
 	}
+
 	fmt.Println("Chom")
 	chom := new(Instance)
 	chom.SlackTeamID = []byte("hello1")
@@ -36,8 +46,8 @@ func initDB(db *bun.DB) (err error) {
 	fmt.Println(res)
 	skz := new(Instance)
 	err = db.NewSelect().
-    Model(skz).
-    Scan(ctx)
+		Model(skz).
+		Scan(ctx)
 	if err != nil {
 		return err
 	}
