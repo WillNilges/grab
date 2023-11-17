86face8d1c824cb75acd92e8d95f09e7aa71e1ec
Will Nilges <wdn5796@rit.edu>
2023-11-17 09:41:44 -0500

    Stub some interfaces and structs

---
+++ chat_platform.go
@@ -0,0 +1,27 @@
+package grab
+
+// A message is Grab's way to interpret something that somebody
+// has said. It will be a unifying agent between all platforms that
+// Grab supports.
+type Message struct {
+	name string
+	handle string
+	timestamp string
+	attachment []Attachment
+}
+
+// Grab doesn't store any user data, but it will be helpful to
+// treat anything that isn't text inside a user's message as an
+// Attachment that links to wherever it's stored.
+type Attachment struct {
+	title string
+	sourceURL string
+	destURL string
+}
+
+// A ChatPlatform is a generic way for Grab to treat any and all
+// apps it is connected with on the input side. This includes
+// Slack, Discord, etc.
+type ChatPlatform interface {
+	getConversation() []Message
+}
---
+++ wiki_platform.go
@@ -0,0 +1,25 @@
+package grab
+
+// Generic types for pages on a customer's wiki. Wiki software is almost
+// certainly too generic for this to be useful, but I guess I can try.
+type WikiPage struct {
+	title string
+	content []WikiSection
+	url string
+}
+
+// A generic section on someone's wiki. This might be more useful than
+// the above, actually, since I can dump transcripts into this.
+type WikiSection struct {
+	title string
+	content string
+}
+
+// A WikiPlatform is Grab's generic interface for any kind of knowledge base.
+// (Such as MediaWiki, BookStack, Confluence, etc)
+// There's not that much that it needs to be able to do generically.
+type WikiPlatform interface {
+	publish() error
+	upload() error
+	fetch() (string, error)
+}
